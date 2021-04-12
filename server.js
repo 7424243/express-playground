8e2d871b03f42d13ad65b6282341d6a61dd7d416
Sam Sheets <sam.sheets94@gmail.com>
2021-04-12 11:49:06 -0600

    req and res and routing

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express()
 
 //when should happen when the browser makes a GET request
 //location: '/'
+//callback function: what happens when the route gets visited
 app.get('/', function(req, res) {
     res.send('<h1>Hello, world!</h1>')
+})
+app.get('/contact', function(req, res) {
+    res.send('Contact me at: sam.sheets94@gmail.com')
+})
+app.get('/about', function(req, res) {
+    res.send('I live in Loveland, CO.')
 })
 
 //listen on a specific port: 3000
