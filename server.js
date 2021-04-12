83cca29f45b41aa9b2b9eb41c8d581c621c53de6
Sam Sheets <sam.sheets94@gmail.com>
2021-04-12 11:38:52 -0600

    working with req and res

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express')
 //create the actual express app
 const app = express()
 
+//when should happen when the browser makes a GET request
+//location: '/'
+app.get('/', function(req, res) {
+    res.send('<h1>Hello, world!</h1>')
+})
+
 //listen on a specific port: 3000
 app.listen(3000, function() {
     console.log('Server started on port 3000')
