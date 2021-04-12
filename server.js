19f40cba1250e9a089beb9e9c8510c215a851469
Sam Sheets <sam.sheets94@gmail.com>
2021-04-12 12:20:39 -0600

    using body-parser and post requests

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 //create the actual express app
 const app = express()
+app.use(bodyParser.urlencoded())
 
 //when should happen when the browser makes a GET request
 //location: '/'
@@ -9,6 +11,13 @@ const app = express()
 //__dirname: the directory name no matter if the server is running locally or via a cloud
 app.get('/', function(req, res) {
     res.sendFile(__dirname + '/index.html')//sending an html file to the browser
+})
+
+app.post('/index.html', function(req, res) {
+    let num1 = Number(req.body.num1)
+    let num2 = Number(req.body.num2)
+    let result = num1 + num2
+    res.send(`The result of the calculation is: ${result}`)
 })
 
 //listen on a specific port: 3000
